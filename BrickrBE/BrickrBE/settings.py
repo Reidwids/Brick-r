06ee83caabd565c9aba927067d8aed5d8a69814e
reidwids <96877197+Reidwids@users.noreply.github.com>
2022-05-12 09:55:40 -0400

    Link database and app urls

--- BrickrBE/BrickrBE/settings.py
+++ BrickrBE/BrickrBE/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'BrickrBE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BrickrBE',
+        'USER': 'postgres'
+
+
     }
 }
 
