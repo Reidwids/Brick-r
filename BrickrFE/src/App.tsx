9809e1cc7ec74d96c050ddf022430a8c564e8525
reidwids <96877197+Reidwids@users.noreply.github.com>
2022-05-12 10:38:10 -0400

    Add home and about routes to FE, change css to scss

--- BrickrFE/src/App.tsx
+++ BrickrFE/src/App.tsx
@@ -1,45 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+	return (
+		<div className="App">
+			<Routes>
+				{/* <Route path="/" element={<Layout />}> */}
+				<Route path="" element={<Home />} />
+				<Route path="about" element={<About />} />
+				{/* </Route> */}
+			</Routes>
+		</div>
+	);
 }
 
-export default App
+export default App;
