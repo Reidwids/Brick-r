f47459b4ef3405a9f7afa42d07b26440b5979755
reidwids <96877197+Reidwids@users.noreply.github.com>
2022-05-13 14:46:08 -0400

    navbar

--- BrickrFE/src/App.tsx
+++ BrickrFE/src/App.tsx
@@ -1,17 +1,38 @@
 import { useState } from 'react';
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import navLogo from './assets/BrickrLogo.svg';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
 function App() {
 	return (
 		<div className="App">
+			<nav className="navbar">
+				<div id="navLS">
+					<Link className="nav-link" to="/">
+						<img id="navLogo" src={navLogo} alt="logo" />
+					</Link>
+				</div>
+				<div id="navRS">
+					<Link className="nav-link" to="/about">
+						About
+					</Link>
+					<Link className="nav-link" to="/login">
+						Login
+					</Link>
+					<Link className="nav-link" to="/signup">
+						Signup
+					</Link>
+				</div>
+			</nav>
 			<Routes>
-				{/* <Route path="/" element={<Layout />}> */}
 				<Route path="" element={<Home />} />
 				<Route path="about" element={<About />} />
-				{/* </Route> */}
+				<Route path="login" element={<Login />} />
+				<Route path="signup" element={<Signup />} />
 			</Routes>
 		</div>
 	);
